56d7a5db77583ee073757a2a6ca4466cddfc4ecc
Oleg-StackOverflow <olegzakharov479@gmail.com>
2024-09-14 23:14:58 +0300

    Update Math.java

--- Laba2/Math.java
+++ Laba2/Math.java
@@ -11,10 +11,21 @@ public class Math {
         System.out.println(result1);
 
         d = 7.89d;
+        a = -0.58d;
 
-        double result2 = java.lang.Math.log(java.lang.Math.abs(2*c/a+java.lang.Math.sqrt(java.lang.Math.tanh(java.lang.Math.abs(b)*c/Logarithm.log(c, d)))));
+        double result2 = java.lang.Math.log(java.lang.Math.abs(2*c/a+java.lang.Math.sqrt(java.lang.Math.tanh(java.lang.Math.abs(b)*c)/Logarithm.log(c, d))));
 
         System.out.println(result2);
+
+        a = 2.54d;
+        b = -1.34;
+        c = 1.05d;
+        d = 0.78d;
+
+        double result3 = Logarithm.log(a, java.lang.Math.abs(c/a*java.lang.Math.sqrt(java.lang.Math.tanh(java.lang.Math.abs(b)*c)/Logarithm.log(java.lang.Math.sqrt(c), d))));
+
+        System.out.println(result3);
+
     }
 
 }
