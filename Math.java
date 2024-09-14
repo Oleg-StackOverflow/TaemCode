b5081264a93b12b1eba15b4ca10e198979056216
OlegZxNeo <olegzakharov479@gmail.com>
2024-09-14 20:44:07 +0300

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,15 @@
+public class Math {
+    public static void main(String[] args) {
+        double a = 0.58d;
+        double b = -0.34d;
+        double c = 1.25d;
+        double d = 0.89d;
+        double result1 = java.lang.Math.sqrt(c)/java.lang.Math.tan(a)+java.lang.Math.sqrt(1/java.lang.Math.tanh(2*c)/Logarithm.log(a, d));
+
+        System.out.println(result1);
+
+        d = 7.89d;
+        System.out.println(d);
+    }
+
+}
