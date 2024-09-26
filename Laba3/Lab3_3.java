0c898dfcdbd7896f77a13d9042d6970cafba4315
Oleg-StackOverflow <olegzakharov479@gmail.com>
2024-09-26 18:18:56 +0300

    3

---
+++ Laba3/Lab3_3.java
@@ -0,0 +1,34 @@
+package Laba3;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class Lab3_3 {
+    public static void main(String[] args) {
+        double epsilon = 1e-5;
+        double result = computeSeries(epsilon);
+        System.out.printf("Сума ряду: %.10f\n", result);
+    }
+
+    public static double computeSeries(double epsilon) {
+        double sumSeries = 0.0;
+        double term;
+        int i = 1;
+
+        do {
+            term = Math.pow(-1, i) / factorial(i);
+            sumSeries += term;
+            i++;
+        } while (Math.abs(term) >= epsilon);
+
+        return sumSeries;
+    }
+
+    public static double factorial(int n) {
+        double result = 1.0;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
