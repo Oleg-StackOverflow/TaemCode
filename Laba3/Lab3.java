1319a3f25147b6a5d71751d7704af3e28ed27a58
Oleg-StackOverflow <olegzakharov479@gmail.com>
2024-09-26 11:08:39 +0300

    2

--- Laba3/Lab3.java
+++ Laba3/Lab3.java
@@ -1,20 +1,31 @@
 package Laba3;
 
+import java.util.Scanner;
+
 public class Lab3 {
     public static void main(String[] args) {
-        double t = -1; //оскільки значення t не задано умовой деякий прикладів було вирішено взяти значення t як -1
-                      // але по лабі треба буде зробити щоб була ще перевірка значень тож це значення тимчасове
-        double s = 1; // як і це значення
-        for(double k = 3; k <= 25; k++) {
-            double summ = 0;
-            double summ_1 = 0;
-            for(double i = 1; i <= k; i++) {
-                summ_1 = summ + Math.log(-t * i)*Math.cos(Math.sqrt(s*(1/Math.pow(i, 2))));
-                summ = summ_1;
-                System.out.println("Значення прикладу при k = " + k + " та при i = " + i +" :" + summ);
-            }
-        }
 
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введіть значення t: ");
+        double t = in.nextDouble();
+        System.out.print("Введіть значення k: ");
+        double k = in.nextDouble();;
+        check(k);
+        System.out.print("Введіть значення s: ");
+        double s = in.nextDouble();
+        double summ = 0;
+        double summ_1 = 0;
+        for(double i = 1; i <= k; i++) {
+            summ_1 = summ + Math.log(-t * i)*Math.cos(Math.sqrt(s*(1/Math.pow(i, 2))));
+            summ = summ_1;
+        }
+        System.out.println("Значення прикладу при k = " + k + ": " + summ);
+
+    }
+    public static void check(double k) {
+        if (k < 3 || k > 25) {
+            throw new IllegalArgumentException("param k = " + k);
+        }
     }
 
 }
