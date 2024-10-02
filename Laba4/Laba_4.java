e5eda406a712cb8d10ca96a21043eb0d780923dd
Oleg-StackOverflow <olegzakharov479@gmail.com>
2024-10-02 21:20:42 +0300

    2

--- Laba4/Laba_4.java
+++ Laba4/Laba_4.java
@@ -1,20 +1,26 @@
 package Laba4;
 
+import java.util.Arrays;
+
 public class Laba_4 {
 
     public static void main(String[] args) {
 
-        int[] Mass_A = {19,6,80,72,89,21,3,62,67,93};
+        int[] Mass_A = {19,6,80,72,89,21,3,82,67,93};
 
-        int max = Mass_A[0];
-        for (int i = 1; i < Mass_A.length; i++) {
-            if (Mass_A[i] > max && Mass_A[i] % 2 == 0) {
-                max = Mass_A[i];
+        int max = MaxArray(Mass_A);
 
-            }
-        }
         System.out.println(max);
 
     }
+    public static int MaxArray(int[] array) {
+        int max_a = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max_a && array[i] % 2 == 0) {
+                max_a = array[i];
 
+            }
+        }
+        return max_a;
+    }
 }
