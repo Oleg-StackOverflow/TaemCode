324b3fb4de54eb35c0d8e43914216beb4b6247b0
Oleg-StackOverflow <olegzakharov479@gmail.com>
2024-10-02 20:41:35 +0300

    Update Laba_4.java

--- Laba4/Laba_4.java
+++ Laba4/Laba_4.java
@@ -4,7 +4,7 @@ public class Laba_4 {
 
     public static void main(String[] args) {
 
-        int[] Mass_A = new int[]{19,6,80,72,89,21,3,62,67,93};
+        int[] Mass_A = {19,6,80,72,89,21,3,62,67,93};
 
         int max = Mass_A[0];
         for (int i = 1; i < Mass_A.length; i++) {
