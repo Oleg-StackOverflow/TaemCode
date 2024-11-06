3e44e2dff1ff27c6fe1d693fefc9d5c55022c4f2
Oleg-StackOverflow <olegzakharov479@gmail.com>
2024-11-06 18:22:46 +0200

    3

---
+++ Laba9/BinaryStringConverter.java
@@ -0,0 +1,10 @@
+package Laba9;
+
+public class BinaryStringConverter {
+    public static int binaryStringToInt(String binaryString) {
+        if (binaryString == null || !binaryString.matches("[01]+")) {
+            throw new IllegalArgumentException("Неправильна бінарна строка");
+        }
+        return Integer.parseInt(binaryString, 2);
+    }
+}
