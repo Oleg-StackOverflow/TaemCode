19a36a91ef2947ab8371832abaf865394c272ad7
Oleg-StackOverflow <olegzakharov479@gmail.com>
2025-02-12 16:57:31 +0200

    Update SelectionSorter.java

--- semester_2/algorithms/Laba_1/SelectionSorter.java
+++ semester_2/algorithms/Laba_1/SelectionSorter.java
@@ -4,21 +4,32 @@ public class SelectionSorter {
     public static void sort(int[] array) {
         long startTime = System.nanoTime();
 
+        int comparisons = 0;
+        int swaps = 0;
+
         for (int i = 0; i < array.length; i++) {
             int pos = i;
             int min = array[i];
             for (int j = i + 1; j < array.length; j++) {
+                comparisons++;
                 if (array[j] < min) {
                     pos = j;
                     min = array[j];
                 }
             }
-            array[pos] = array[i];
-            array[i] = (short) min;
+            if (pos != i) {
+                int temp = array[i];
+                array[i] = array[pos];
+                array[pos] = temp;
+                swaps++;
+            }
         }
 
         long endTime = System.nanoTime();
-        double elapsedTime = (endTime - startTime) / 1_000_000_000.0; // Переведення в секунди
+        double elapsedTime = (endTime - startTime) / 1_000_000_000.0;
+
         System.out.println("Час виконання сортування: " + elapsedTime + " секунд");
+        System.out.println("Кількість порівнянь: " + comparisons);
+        System.out.println("Кількість перестановок: " + swaps);
     }
 }
