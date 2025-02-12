4d77c488e2b15c150bfd144b693fd507dfb62b0d
Oleg-StackOverflow <olegzakharov479@gmail.com>
2025-02-12 15:54:05 +0200

    Update SelectionSorter.java

--- semester_2/algorithms/Laba_1/Laba1.java
+++
@@ -1,4 +0,0 @@
-package semester_2.algorithms.Laba_1;
-
-public class Laba1 {
-}
---
+++ semester_2/algorithms/Laba_1/SelectionSorter.java
@@ -0,0 +1,24 @@
+package semester_2.algorithms.Laba_1;
+
+public class SelectionSorter {
+    public static void sort(short[] array) {
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < array.length; i++) {
+            int pos = i;
+            int min = array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    pos = j;
+                    min = array[j];
+                }
+            }
+            array[pos] = array[i];
+            array[i] = (short) min;
+        }
+
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Час виконання сортування: " + elapsedTime + " наносекунд");
+    }
+}
